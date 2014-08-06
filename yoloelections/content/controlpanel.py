09779355886dc9cf566c7609fa61bf0b6b57d99b
Steve McMahon <steve@dcn.org>
2014-08-06 14:33:17 -0700

    Add election result type

---
+++ yoloelections/content/controlpanel.py
@@ -0,0 +1,44 @@
+"""
+
+    Define add-on settings.
+
+"""
+
+from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
+from plone.app.registry.browser.controlpanel import RegistryEditForm
+from plone.directives import form
+from plone.memoize.view import memoize_contextless
+from plone.registry.interfaces import IRegistry
+from zope import schema
+from zope.component import getUtility
+
+
+class IElectionsSettings(form.Schema):
+    """ Define settings data structure """
+
+    form.widget(upcoming_elections='z3c.form.browser.textlines.TextLinesFieldWidget')
+    upcoming_elections = schema.List(
+        title=u'Upcoming Elections',
+        description=u"""
+            Set the list of upcoming elections for the "next_election" field.
+            Format is code|title, one per line; e.g., G2020|November, 2020""",
+        required=False,
+        default=['None|Unscheduled'],
+        value_type=schema.ASCIILine(title=u'Election'),
+    )
+
+
+class SettingsEditForm(RegistryEditForm):
+    """
+    Define form
+    """
+    schema = IElectionsSettings
+    label = u"Elections settings"
+
+
+class SettingsControlPanel(ControlPanelFormWrapper):
+    """
+    View
+    """
+    form = SettingsEditForm
+
