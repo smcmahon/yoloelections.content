35eb3bbc7a94adabe5e57328ab3ef7651a3a64a7
Steve McMahon <steve@dcn.org>
2014-10-01 16:22:30 -0700

    Select by ballot

--- yoloelections/content/voter_guide.py
+++ yoloelections/content/voter_guide.py
@@ -4,14 +4,17 @@ from Products.Five import BrowserView
 
 import csv
 import cStringIO
+import re
 
-# District, Office; Candidate/Title; Designation; Party preference; Statement Filename
+# District, Office; Candidate/Title; Designation; Party preference; Statement Filename; ballots
 
 
 class VoterGuideView(BrowserView):
     """ support for voter guide template """
 
-    def guideItems(self, ballot=0):
+    def guideItems(self):
+
+        target_ballot = int(self.request.get('ballot', '0'))
 
         reader = csv.reader(cStringIO.StringIO(self.context.guide_data.data), dialect='excel')
         # import pdb; pdb.set_trace()
@@ -26,6 +29,18 @@ class VoterGuideView(BrowserView):
             doffice, candidate, desig, party, statement, ballots = [s.strip().decode('utf8', 'replace') for s in row]
             if not candidate:
                 continue
+
+            if target_ballot:
+                my_ballots = []
+                for ballot in [s.strip() for s in re.split(r"[, ]", ballots)]:
+                    if '-' in ballot:
+                        start, end = ballot.split('-')
+                        my_ballots += range(int(start), int(end) + 1)
+                    else:
+                        my_ballots.append(int(ballot))
+                if target_ballot not in my_ballots:
+                    continue
+
             if doffice != last_doffice:
                 last_doffice = doffice
                 candidates = []
@@ -33,21 +48,16 @@ class VoterGuideView(BrowserView):
                     'doffice': doffice,
                     'candidates': candidates,
                     })
+            if candidate.startswith('Measure'):
+                label = 'Analysis, text and arguments'
+            else:
+                label = 'Statement'
             candidates.append({
                     'candidate': candidate,
                     'desig': desig,
                     'party': party,
                     'statement': statement,
+                    'label': label,
                 })
 
-#            my_ballots = []
-#            for ballot in [s.strip() for s in ballots.split(',')]:
-#                if '-' in ballot:
-#                    start, end = ballot.split('-')
-#                    my_ballots += range(int(start), int(end)+1)
-#                else:
-#                    my_ballots.append(int(ballot))
-#                my_ballots.append(ballot)
-#            rez.append([doffice, title, desig, pary, statement, ballots,
-#                        my_ballots])
         return offices
