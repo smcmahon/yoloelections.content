8e2b9c4cd12e6157a6b36376257bd8915c8deee8
Steve McMahon <steve@dcn.org>
2018-11-03 13:39:33 -0700

    paginate

--- yoloelections/content/return_page.py
+++ yoloelections/content/return_page.py
@@ -9,7 +9,7 @@ import cStringIO
 
 
 fields = {
-    'Page': 'Page',
+    'Page': 'page',
     'Contest #': 'contest',
     'Contest Name': 'contest_name',
     'Cadn/choice name': 'choice_name',
@@ -65,10 +65,29 @@ def commaize(anint):
         return "%d" % anint
 
 
+def entitle(contest):
+    contest = contest.title()
+    contest = contest.replace(' The ', ' the ')
+    contest = contest.replace(' A ', ' a ')
+    contest = contest.replace(' Of ', ' of ')
+    contest = contest.replace('1St ', '1st ')
+    contest = contest.replace('2Nd ', '2nd ')
+    contest = contest.replace('3Rd ', '3rd ')
+    contest = contest.replace('4Th ', '4th ')
+    contest = contest.replace('5Th ', '5th ')
+    contest = contest.replace('6Th ', '6th ')
+    contest = contest.replace('7Th ', '7th ')
+    contest = contest.replace('8Th ', '8th ')
+    contest = contest.replace('Usd', 'USD')
+    contest = contest.replace('Jusd', 'JUSD')
+    contest = contest.replace('Ws ', 'WS ')
+    return contest
+
+
 class ReturnPageView(BrowserView):
     """ support for return page template """
 
-    def contests(self):
+    def pages(self):
         reader = csv.DictReader(
             cStringIO.StringIO(utf16Fix(self.context.return_data.data)),
             dialect='excel'
@@ -87,14 +106,24 @@ class ReturnPageView(BrowserView):
                         rez[fixed_key] = row[akey]
             rows.append(rez)
         rows.sort(contest_cmp)
-        contests = []
+        pages = []
+        last_page = ''
         last_contest = 0
         for row in rows:
+            page = row['page']
+            if page != last_page:
+                last_page = page
+                contests = []
+                pages.append(dict(
+                    page_title=entitle(page),
+                    page_id=page.replace(' ', '_'),
+                    contests=contests,
+                ))
             contest_no = row['contest']
             if contest_no != last_contest:
                 last_contest = contest_no
                 this_contest = dict(
-                    contest_name=row['contest_name'],
+                    contest_name=entitle(row['contest_name']),
                     precincts=row['precincts'],
                     prec_reporting=row['prec_reporting'],
                     ballots_cast=row['ballots_cast'],
@@ -103,9 +132,9 @@ class ReturnPageView(BrowserView):
                 )
                 contests.append(this_contest)
             this_contest['choices'].append(dict(
-                choice_name=row['choice_name'],
+                choice_name=row['choice_name'].title(),
                 party=row['party'],
                 votes=commaize(row['votes']),
                 klass=(row['options'] >= row['rank']) and 'leading' or None,
             ))
-        return contests
+        return pages
